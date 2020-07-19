7561ff5a4a04ffeb6562156b499940947ab8b3e0
Shay Halfon <shayh@biyond.co.il>
2020-07-19 13:51:10 +0300

    updated docstrings for function

--- functions/functions.php
+++ functions/functions.php
@@ -198,6 +198,10 @@ function getMessages( $user_id, $only_not_opened = true ) {
 
 
 function setMessagesStatus( $from_user_id, $to_user_id ) {
+    /*
+     * Function used to update the message that user read
+     *
+     * */
     global $mysqli;
     $update = "UPDATE `messages` SET `is_opened`=1 WHERE `from_user_id`=$from_user_id AND `to_user_id`=$to_user_id AND `is_opened`=0";
     $mysqli->query( $update );
@@ -261,7 +265,8 @@ function addNewTrip(
     $places_to_eat,
     $hotels,
     $good_to_know,
-    $thing_to_give_up ) {
+    $thing_to_give_up)
+{
     /*
      * Inserting a new trip the user shared into the trips table
      *
@@ -271,10 +276,14 @@ function addNewTrip(
                           (user_id, destination_id, type_id, theme_id, season_id, age_range_id, price_id, trip_story,recommended_attractions, places_to_eat, hotels, good_to_know, thing_to_give_up)
                     VALUES($user_id, $destination_id, $type_id, $theme_id, $season_id, $age_range_id, $price_id, '$trip_story', '$recommended_attractions', '$places_to_eat', '$hotels', '$good_to_know', '$thing_to_give_up')";
 
-    $mysqli->query( $insert );
+    $mysqli->query($insert);
 }
 
 function getRelation( $relation_type, $from, $to ) {
+    /*
+     * Get the relation value between for season, type, theme, price, age
+     *
+     * */
     global $mysqli;
     $result = $mysqli->query( "SELECT `value` FROM `relations` WHERE `relation_type`='$relation_type' AND `from_relation`=$from AND `to_relation`=$to LIMIT 1" );
     return $result->fetch_assoc();
@@ -291,6 +300,10 @@ function getUserTrips($user_id){
 }
 
 function getUserName($trip_id){
+    /*
+     * Function to get the name and last name of a user based on trip_id
+     * !!NOT IN USE ANYMORE!!
+     * */
     global $mysqli;
     $sql = "select CONCAT(u.first_name, \" \", u.last_name) as \"name\"
             from users u INNER JOIN trips t on u.id = t.user_id
@@ -301,6 +314,11 @@ function getUserName($trip_id){
 }
 
 function getResultForSeason($join_on, $field_name,$history_col,$table_name) { // $search_type => type_id, theme_id, season_id....
+    /*
+     * Function used for the first report to count how many times people searched for each result of a parameter.
+     * For example for Type parameter how many times they searched for
+     *
+     * */
     global $mysqli;
 
     $sql = "select `$field_name`, COALESCE(count(search_history.$history_col),0) AS `count`
@@ -313,6 +331,10 @@ function getResultForSeason($join_on, $field_name,$history_col,$table_name) { //
 
 
 function getResultForMonthlyRegistered(){
+    /*
+     * Function used for the second report, get monthly registered user for year 2020 and order by month
+     *
+     * */
     global $mysqli;
 
     $sql = "SELECT DATE_FORMAT(u.register_date, '%M') as \"month\",
@@ -328,6 +350,10 @@ function getResultForMonthlyRegistered(){
 }
 
 function getTripsAdded(){
+    /*
+     * Function used for the third report, get monthly added trips for year 2020 and order by
+     *
+     * */
     global $mysqli;
 
     $sql = "SELECT EXTRACT(MONTH from t.date_added) as \"month\",
